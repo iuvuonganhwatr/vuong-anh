cb2c1aa185a7186525cfba5413f779779fd5a81c
iuvuonganhwatr <105943046+iuvuonganhwatr@users.noreply.github.com>
2022-05-21 01:31:45 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,20 +1,17 @@
 const CONFIG = {
-    titleWeb: "Tiêu đề web",
-    introTitle: 'Tên người yêu bạn',
-    introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
-    Đằng sau chữ yêu đây là thương`,
-    btnIntro: '^^HiHi^^',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 🥰',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Thích lắm <33',
-    btnNo: 'Không nha :3',
-    question: 'Trên thế giới hơn 7 tỉ người mà sao bạn lại yêu mình <3',
-    btnReply: 'Gửi cho bạn <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Mình biết mà 🥰. Yêu bạn nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h, mình qua đón đi chơi nha.',
-    btnAccept: 'Okiiiii lun <3',
-    messLink: 'https://github.com/zukahai/Confess-Crush' //link mess của các bạn. VD: https://www.facebook.com/messages/t/100014188333536
+    titleWeb: "Gui a iu",
+    introTitle: 'chao Vuong Anh',
+    introDesc: `a chao e chua`,
+    btnIntro: 'iem chao chiiii ak',
+    title: 'a ne 🥰',
+    desc: 'a coa thic an cuc ko',
+    btnYes: 'Thicc lắm <33',
+    btnNo: 'Hong nha :3',
+    question: 'z sao a lai iu e z <3',
+    btnReply: 'Gui cho e iu <3',
+    reply: 'vi e wa de thuong chu seooooo <33333333',
+    mess: 'e bit ma 🥰. e iu a nhiu 😘😘',
+    messDesc: 'Sang mai 8h, a qua don be ik choi nha.',
+    btnAccept: 'Okiiiii lun be iu cua a <3',
+    messLink: 'https://github.com/zukahai/cunconlanhloi' //link mess của các bạn. VD: https://www.facebook.com/messages/t/100014188333536
 }
